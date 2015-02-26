5f6d44741dad5d02b9f1465732215fbb20429601
Adam Prager <prager.adam87@gmail.com>
2015-02-26 22:05:16 +0100

    Fixed some travis issues

--- Tests/Traits/TraitTest.php
+++ Tests/Traits/TraitTest.php
@@ -2,11 +2,7 @@
 
 namespace Padam87\AttributeBundle\Tests\Traits;
 
-use Symfony\Bundle\FrameworkBundle\Command\CacheClearCommand;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\ConsoleOutput;
 
 class TraitTest extends WebTestCase
 {
